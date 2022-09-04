631a74a7036d667b86429023a2b9d27e4bbfd9fc
mirac <miracerdin@gmail.com>
2022-09-04 16:19:22 +0300

    api changed

--- src/redux/store/thunk/newsThunk.jsx
+++ src/redux/store/thunk/newsThunk.jsx
@@ -3,16 +3,15 @@
 import { setLoading, clearLoading } from "../../actions/appActions";
 import axios from "axios";
 import { setNewsList } from "../../actions/newsActions";
-const url =
-  "https://newsapi.org/v2/everything?q=apple&from=2022-09-03&sortBy=popularity&apiKey=04c4a30ac54643cb9d41833956cb3421";
+const url = "https://jsonplaceholder.typicode.com/posts";
 
 // Bu kullanımda getNew bir değişken gibi düşünülebilir. Dolayısıyla view tarafında dispatch(getNews) şeklinde yazılır.
 const getNews = async (dispatch) => {
   try {
     dispatch(setLoading());
     const { data } = await axios.get(url);
-    dispatch(setNewsList(data.articles));
-    console.log(data.articles);
+    dispatch(setNewsList(data));
+    console.log(data);
   } catch (error) {
     console.log(error);
   } finally {
